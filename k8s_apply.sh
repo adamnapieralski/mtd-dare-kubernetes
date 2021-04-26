80e3f022e7f858f89e6639d0f754074fb98a2712
kost13 <l.kostrzewa12@gmail.com>
2021-04-26 23:45:03 +0200

    add simple mtd based on patches

--- k8s_apply.sh
+++ k8s_apply.sh
@@ -1,7 +1,4 @@
-kubectl apply -f service-nginx.yaml
-kubectl apply -f service-apache.yaml
+kubectl apply -f service.yaml
 
 kubectl apply -f deployment-nginx.yaml
 kubectl apply -f deployment-apache.yaml
-
-kubectl apply -f ingress.yaml
