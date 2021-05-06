bb92414b73df9116a96c26488e41bcdfce983282
kost13 <l.kostrzewa12@gmail.com>
2021-05-06 08:24:39 +0200

    move back and front-end to different services

---
+++ new/svc_mtd.py
@@ -0,0 +1,49 @@
+import time
+import sys
+import subprocess
+
+svc_file = 'service-frontend.yaml'
+svc_name = 'frontend'
+
+def patches_generator():
+    files = ['patch-apache.yaml', 'patch-nginx.yaml']
+    i = 0
+    while True:
+        i = (i + 1) % 2
+        yield files[i]
+
+def run_command(command):
+    print("command:", command)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    print("output:", output.decode('utf-8'))
+    return error is not None
+
+def init():
+    return run_command('./k8s_apply.sh')
+
+def patch(patch_file):
+    return run_command('kubectl patch svc {} -p "$(cat {})"'.format(svc_name, patch_file))
+
+def run_mtd(dt):
+    if not init():
+        print('Initial error')
+        return
+    time.sleep(20)
+
+    patches = patches_generator()
+
+    while True:
+        patch(next(patches))
+        time.sleep(dt)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        dt = int(sys.argv[1])
+    else:
+        dt = 10
+
+    run_mtd(dt)
+
+
+
