b7ab0aa6189a91f0b7876745675545393a32855f
adamnapieralski <adam.napieralski@gmail.com>
2021-05-07 08:00:00 +0200

    add ingress patching & requests tests

---
+++ new/requests_test.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import sys
+from requests_html import HTMLSession
+
+url = "http://192.168.49.2:31144/"
+
+def make_request():
+    r = requests.get(url)
+    server = r.headers['Server']
+    print(server)
+
+def make_request_with_js():
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render()
+    server = r.headers['Server']
+    print(server)
+
+def run_requests(interval_ms, with_js):
+    while True:
+        if with_js:
+            make_request_with_js()
+        else:
+            make_request()
+        time.sleep(interval_ms / 1000.)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        dt = int(sys.argv[1])
+    else:
+        dt = 500
+
+    run_requests(dt, True)
