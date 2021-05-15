edc774f9f95a5e5577790aff01289e042b58537f
kost13 <l.kostrzewa12@gmail.com>
2021-05-15 20:57:13 +0200

    add wordpress

--- clear_k8.sh
+++ clear_k8.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 kubectl delete svc --all
 kubectl delete deployment --all
 kubectl delete pod --all
+kubectl delete ingress --all
+
+if [ $1 == 'pv' ]
+then
+    kubectl delete pvc --all
+    kubectl delete pv --all
+fi
---
+++ wordpress/k8s_apply.sh
@@ -0,0 +1,4 @@
+kubectl apply -k ./
+
+kubectl apply -f ingress.yaml
+kubectl apply -f ingress-config.yaml -n kube-system
