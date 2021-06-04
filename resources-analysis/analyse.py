defbe44ae69211b224099aae3f96d5b2be5ec6b1
adamnapieralski <adam.napieralski@gmail.com>
2021-06-04 12:02:08 +0200

    small fix in resource-analysis/analyse.py

--- resources-analysis/analyse.py
+++ resources-analysis/analyse.py
@@ -13,25 +13,6 @@ servers = [
 
 def get_data_series(filepath):
     df = pd.read_json(filepath)
-    df['pod'] = df['pod'].apply(lambda x: re.match(r'^(\w+-\w+)-.*', x).groups()[0])
-    group_series = df.groupby(['timestamp', 'pod'])['cpu', 'memory'].sum()
-
-    pods = np.array([x[1] for x in group_series.index])
-
-    data_series = {}
-    for server in servers:
-        ids = np.where(pods == server['pod'])[0]
-        data_series[server['name']] = {}
-        for type in ['cpu', 'memory']:
-            data_series[server['name']][type] = list(group_series[type][ids])
-
-        timestamps = [t[0] for t in group_series['cpu'][ids].index]
-        dt = math.floor((timestamps[1] - timestamps[0]).total_seconds())
-        data_series[server['name']]['seconds'] = [i * dt for i in range(len(timestamps))]
-    return data_series
-
-def get_data_series_new(filepath):
-    df = pd.read_json(filepath)
     df['pod'] = df['pod'].apply(lambda x: re.match(r'^(\w+)-.*', x).groups()[0])
     group_series = df.groupby(['timestamp', 'pod'])['cpu', 'memory'].sum()
 
@@ -47,27 +28,7 @@ def get_data_series_new(filepath):
 
     return data_series
 
-def plot_series(data_series, server_name, title_info, filepath=''):
-    fig, ax = plt.subplots()
-    l1 = ax.plot(data_series[server_name]['seconds'], data_series[server_name]['cpu'], label='CPU', color='red')
-    ax.set_ylabel("CPU []", color='red')
-    ax.set_xlabel('Time [s]')
-    ax2 = ax.twinx()
-    l2 = ax2.plot(data_series[server_name]['seconds'], data_series[server_name]['memory'], label='memory', color='blue')
-    ax2.set_ylabel("Memory [Mb]", color="blue")
-
-
-    lns = l1+l2
-    labs = [l.get_label() for l in lns]
-    ax.legend(lns, labs, loc=0)
-
-    ax.set_title('Server: {} - {}'.format(server_name, title_info))
-    # plt.show()
-    if filepath != '':
-        fig.savefig(filepath)
-
-
-def plot_series_new(data_series, title, filepath=''):
+def plot_series(data_series, title, filepath=''):
     fig, ax = plt.subplots()
     l1 = ax.plot(data_series['seconds'], data_series['cpu'], label='CPU', color='red')
     ax.set_ylabel("CPU [%]", color='red')
@@ -87,7 +48,7 @@ def plot_series_new(data_series, title, filepath=''):
         fig.savefig(filepath)
 
 def plot_multiple_series(files, title, resource_type='cpu', type='plain'):
-    data_series = [get_data_series_new(f) for f in files]
+    data_series = [get_data_series(f) for f in files]
 
     fig, ax = plt.subplots()
     # secs = data_series[0]['seconds']
@@ -123,9 +84,9 @@ if __name__ == "__main__":
     # else:
     #     raise Exception("No source")
 
-    # data_series = get_data_series_new(filepath)
+    # data_series = get_data_series(filepath)
 
-    # plot_series_new(data_series, 'MTD Deployment (interval 15s, with requests load)', filepath.replace('json', 'png'))
+    # plot_series(data_series, 'MTD Deployment (interval 15s, with requests load)', filepath.replace('json', 'png'))
     # plot_series(data_series, 'nginx', 'info', filepath.replace('json', '{}.png'.format('nginx')))
 
     files_no_req = [
@@ -151,6 +112,3 @@ if __name__ == "__main__":
 
     plot_multiple_series(files_no_req, 'CPU usage for plain MTDs', resource_type='cpu', type='plain')
     plot_multiple_series(files_no_req, 'Memory usage for plain MTDs', resource_type='memory', type='plain')
-
-
-
