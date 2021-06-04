44bee3fc7186304dd0282c02425f4f60ed564b15
adamnapieralski <adam.napieralski@gmail.com>
2021-06-04 12:05:21 +0200

    initial cleanup

--- k8s_apply.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f service.yaml
-
-kubectl apply -f deployment-nginx.yaml
-kubectl apply -f deployment-apache.yaml
--- single-deployment/k8s_apply.sh
+++
@@ -1,2 +0,0 @@
-kubectl apply -f service.yaml
-kubectl apply -f deployment.yaml
