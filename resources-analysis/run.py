b5ab364c84f686767976dd40af11a77371e43342
adamnapieralski <adam.napieralski@gmail.com>
2021-06-04 11:59:59 +0200

    add final resource-analysis changes

--- resources-analysis/run.py
+++ resources-analysis/run.py
@@ -4,9 +4,6 @@ import threading
 
 if __name__ == "__main__":
     # filename_info = "mtd-ingress.i15.no-req.0-mtd-0"
-    filename_info = "mtd-ingress.i15.with-req.mtd"
+    filename_info = "mtd-deployment.i60.with-req.mtd"
     thread = threading.Thread(target=pm.run, args=(60*15, filename_info))
     thread.start()
-    # rtc.run(5, 5000, 120, filename_info)
-    # pm.run(120, "mtd-ingress.i15.req.tn10.i5000")
-
